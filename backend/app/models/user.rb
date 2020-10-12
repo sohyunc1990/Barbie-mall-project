863da5d17c8590ca65b2f9a98e68a1b001f4054f
sohyun <sohyunc1990@gmail.com>
2020-10-12 18:02:50 -0500

    authentication done

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,18 @@ class User < ApplicationRecord
     has_many :favorites
     has_many :reviews
     has_many :products, through: :favorites
+
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
+
+    attr_accessor :password
+
+  def authenticate(plaintext_password)
+    if BCrypt::Password.new(self.password_digest) == plaintext_password
+      self
+    else
+      false
+    end
+end
+
 end
